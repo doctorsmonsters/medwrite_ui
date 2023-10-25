410024f925a9fd32f0cf8bca3c0e41c9fd51157d
Junaid Maqbool <lucifermjm@gmail.com>
2023-10-25 13:18:02 +0500

    changes base url to clien't backend

--- src/Constans/Api.js
+++ src/Constans/Api.js
@@ -1,5 +1,6 @@
 // personal backend url
-export const API_BASE_URL = "https://medwriter.vercel.app/api";
+export const API_BASE_URL = "https://medwriter-backend.vercel.app/api";
+// export const API_BASE_URL = "http://localhost:8000/api";
 export const GOOGLE_CLIENT_ID =
   "945970413385-d8a3jbt748cinsr54k0klctnrfgkno5h.apps.googleusercontent.com";
 export const TINYMCE_API_KEY = "gef8xiyvs164fqxpstv7gkqlfzzfveno0wbcqld4etd71mkd"
