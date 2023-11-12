bb0bf4ff50c4a36bda1dd90d28ad2e9c84772c3d
usama idrees <usamachaudhary12@gmail.com>
2023-11-12 17:11:45 +0500

    deleting Loader added successfully

--- src/Components/Drawer/ReferenceDrawer/ReferenceDrawer.js
+++ src/Components/Drawer/ReferenceDrawer/ReferenceDrawer.js
@@ -7,12 +7,18 @@ import { removeHTMLTags } from "../../../Constans/Helpers";
 
 const ReferenceDrawer = ({ open, setOpen, article }) => {
   const [showFullDescription, setShowFullDescription] = React.useState(null)
+  const [deletingReferenceindex, setdeletingReferenceindex] = React.useState(null)
 
   const toggleDescription = (index) => {
     setShowFullDescription(showFullDescription === index ? null : index)
   }
 
-  const deleteDiscriptionMutation = useMutation({
+  const deleteReference = (id, index) => {
+    setdeletingReferenceindex(index)
+    deleteReferenceMutation.mutate(id)
+  }
+
+  const deleteReferenceMutation = useMutation({
     mutationFn: (data) =>
       deleteReferences(data)
         .then((res) => {
@@ -20,11 +26,12 @@ const ReferenceDrawer = ({ open, setOpen, article }) => {
         })
         .catch((error) => {
           return error
-        }),
+        })
+        .finally(() => setdeletingReferenceindex(null)),
   });
 
   const referenceQuery = useQuery({
-    queryKey: ["references", deleteDiscriptionMutation.data],
+    queryKey: ["references", deleteReferenceMutation.isSuccess],
     queryFn: () =>
       getReferences({ article_id: article }).then((res) => res.data.data),
   });
@@ -69,6 +76,7 @@ const ReferenceDrawer = ({ open, setOpen, article }) => {
                 const { abstract_text, title, id } = item
                 const abstractText = removeHTMLTags(abstract_text)
                 const abstractTextRefactored = abstractText.slice(0, 200);
+                console.log(deletingReferenceindex === id);
                 return (
                   <Box
                     component="div"
@@ -96,17 +104,17 @@ const ReferenceDrawer = ({ open, setOpen, article }) => {
                       </span>
                       <span
                         className=" absolute right-[5.5rem] bottom-0 cursor-pointer bg-black rounded-t-md  text-sm px-2 py-1 hover:bg-gray-600  text-white"
-                        onClick={ () => deleteDiscriptionMutation.mutate(id) }
+                        onClick={ () => deleteReference(id, index) }
                       >
                         Delete
                       </span>
 
+                      { deleteReferenceMutation.isLoading && deletingReferenceindex === index && (
+                        <div className="flex items-center  justify-center my-5">
+                          <Loading />
+                        </div>
+                      ) }
                     </div>
-                    { deleteDiscriptionMutation.isLoading && (
-                      <div className="flex items-center justify-center my-12">
-                        <Loading />
-                      </div>
-                    ) }
                   </Box>
                 );
               }) }
